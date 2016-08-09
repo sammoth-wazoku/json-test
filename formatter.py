417001460af7dec0ed4cadc4b6e05fe7063d8bf7
sammoth <sam.griffiths@wazoku.com>
2016-08-09 17:14:56 +0100

    sort the lists

--- formatter.py
+++ formatter.py
@@ -9,13 +9,23 @@ idea_leaderboard = {'items': []}
 user_leaderboard = {'items': []}
 challenge_leaderboard = {'items': []}
 
+data = sorted(data, key=lambda k: k['num_ideas'], reverse=True)
 for company in data:
     idea_funnel['item'].append({'label': company['name'], 'value': company['num_ideas']})
-    user_funnel['item'].append({'label': company['name'], 'value': company['num_users']})
-    challenge_funnel['item'].append({'label': company['name'], 'value': company['num_challenges']})
     idea_leaderboard['items'].append({'label': company['name'], 'value': company['num_ideas']})
+
+data = sorted(data, key=lambda k: k['num_users'], reverse=True)
+for company in data:
+    user_funnel['item'].append({'label': company['name'], 'value': company['num_users']})
     user_leaderboard['items'].append({'label': company['name'], 'value': company['num_users']})
+
+data = sorted(data, key=lambda k: k['num_challenges'], reverse=True)
+for company in data:
+    challenge_funnel['item'].append({'label': company['name'], 'value': company['num_challenges']})
     challenge_leaderboard['items'].append({'label': company['name'], 'value': company['num_challenges']})
+
+sorted(idea_leaderboard['items'])
+
 
 json.dump(idea_funnel, open('idea_funnel.json', 'w+'))
 json.dump(user_funnel, open('user_funnel.json', 'w+'))
