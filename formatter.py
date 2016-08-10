cbfb8ded674469e49b750b94aa0ce5c8daf9fa48
sammoth <sam.griffiths@wazoku.com>
2016-08-10 11:32:10 +0100

    totals and that

--- formatter.py
+++ formatter.py
@@ -2,34 +2,74 @@ import json
 
 data = json.load(open('dashboard.json'))
 
-idea_funnel = {'item': []}
-user_funnel = {'item': []}
-challenge_funnel = {'item': []}
-idea_leaderboard = {'items': []}
-user_leaderboard = {'items': []}
-challenge_leaderboard = {'items': []}
+## Totals
+
+total_ideas = 0
+total_challenges = 0
+total_users = 0
+
+for company in data:
+    total_ideas += int(company['num_ideas'])
+    total_challenges += int(company['num_challenges'])
+    total_users += int(company['num_users'])
+
+json.dump(
+    {
+        'item': total_ideas,
+        'min': {'value': 0},
+        'max': {'value': (total_ideas * 2)}
+    },
+    open('idea_gekometer.json', 'w+')
+)
+
+json.dump(
+    {
+        'item': total_challenges,
+        'min': {'value': 0},
+        'max': {'value': (total_challenges * 2)}
+    },
+    open('challenge_gekometer.json', 'w+')
+)
+
+json.dump(
+    {
+        'item': total_users,
+        'min': {'value': 0},
+        'max': {'value': (total_users * 2)}
+    },
+    open('user_gekometer.json', 'w+')
+)
+
+
+## Ideas
 
 data = sorted(data, key=lambda k: k['num_ideas'], reverse=True)
+idea_funnel = {'item': []}
+idea_leaderboard = {'items': []}
 for company in data:
     idea_funnel['item'].append({'label': company['name'], 'value': company['num_ideas']})
     idea_leaderboard['items'].append({'label': company['name'], 'value': company['num_ideas']})
+json.dump(idea_funnel, open('idea_funnel.json', 'w+'))
+json.dump(idea_leaderboard, open('idea_leaderboard.json', 'w+'))
+
+## Users
 
 data = sorted(data, key=lambda k: k['num_users'], reverse=True)
+user_funnel = {'item': []}
+user_leaderboard = {'items': []}
 for company in data:
     user_funnel['item'].append({'label': company['name'], 'value': company['num_users']})
     user_leaderboard['items'].append({'label': company['name'], 'value': company['num_users']})
+json.dump(user_funnel, open('user_funnel.json', 'w+'))
+json.dump(user_leaderboard, open('user_leaderboard.json', 'w+'))
+
+## Challenge
 
 data = sorted(data, key=lambda k: k['num_challenges'], reverse=True)
+challenge_funnel = {'item': []}
+challenge_leaderboard = {'items': []}
 for company in data:
     challenge_funnel['item'].append({'label': company['name'], 'value': company['num_challenges']})
     challenge_leaderboard['items'].append({'label': company['name'], 'value': company['num_challenges']})
-
-sorted(idea_leaderboard['items'])
-
-
-json.dump(idea_funnel, open('idea_funnel.json', 'w+'))
-json.dump(user_funnel, open('user_funnel.json', 'w+'))
 json.dump(challenge_funnel, open('challenge_funnel.json', 'w+'))
-json.dump(idea_leaderboard, open('idea_leaderboard.json', 'w+'))
-json.dump(user_leaderboard, open('user_leaderboard.json', 'w+'))
 json.dump(challenge_leaderboard, open('challenge_leaderboard.json', 'w+'))
